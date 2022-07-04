ad71b2e0c153796eecf031610e26a4b97a691a2f
hendrastore <102216067+hendrastore@users.noreply.github.com>
2022-07-04 20:38:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -41,33 +41,33 @@ let wibh = moment.tz('Asia/Jakarta').format('HH')
     
 
 /*============== SOCIAL ==============*/
-global.sig = 'https://instagram.com/lampu_philips267'
-global.sgh = 'https://github.com/BaraXD'
-global.sgc = 'https://chat.whatsapp.com/JXlb6mMlQai77uQSXcmv5T'
-global.sdc = 'https://t.me/BotzzMeeOFC'
+global.sig = 'https://www.instagram.com/hendra.mllb/'
+global.sgh = ''
+global.sgc = 'https://chat.whatsapp.com/IqZke0cAG6G3iVsnB9myfL'
+global.sdc = ''
 global.snh = 'https://nhentai.net/g/365296/'
 
 /*============== PAYMENT ==============*/
-global.pdana = '083174508962'
-global.povo = '083174508962'
-global.pgopay = '083174508962'
-global.ppulsa = '083174508962'
-global.ppulsa2 = '08979530736'
-global.psaweria = 'https://saweria.co/BaraXDz'
+global.pdana = '083181085070'
+global.povo = '083181085070'
+global.pgopay = '083181085070'
+global.ppulsa = '083181085070'
+global.ppulsa2 = '083181085070'
+global.psaweria = ''
 
 /*============== NOMOR ==============*/
-global.nomorbot = '628979530736'
-global.nomorown = '6283174508962'
-global.namebot = 'BotzzMee-Md【﻿ＸｘＹｚ】'
-global.nameown = 'BaraXD'
+global.nomorbot = '24105166540'
+global.nomorown = '24105166540'
+global.namebot = '𝐻𝑒𝑛𝑑𝑟𝑎-ᴍᴅ᭄࿐【﻿ＸｘＹｚ】'
+global.nameown = '𝐻𝑒𝑛𝑑𝑟𝑎-ᴍᴅ᭄࿐'
 
 
 /*============== STAFF ==============*/
 global.owner = [
-  ['6283174508962'],
-  ['6283174508962'],
-  ['6283174508962'],
-  ['6283174508962', 'BotzzMee-Md【﻿ＸｘＹｚ】', true]
+  ['24105166540'],
+  ['24105166540'],
+  ['24105166540'],
+  ['6283174508962', '𝐻𝑒𝑛𝑑𝑟𝑎-ᴍᴅ᭄࿐【﻿ＸｘＹｚ】', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -112,13 +112,13 @@ global.APIKeys = { // APIKey Here
 
 
 /*============== WATERMARK ==============*/
-global.wm = 'BotzzMee-Md【﻿ＸｘＹｚ】' //Main Watermark
-global.wm2 = '¸„٭⊹•~⍣°”ˆ˜¨ BotzzMee-Md¨˜ˆ”°⍣~•⊹٭„¸'
-global.wm3 = '☘︎ ʙᴏᴛᴢᴢᴍᴇᴇ-MD'
+global.wm = '𝐻𝑒𝑛𝑑𝑟𝑎-ᴍᴅ᭄࿐【﻿ＸｘＹｚ】' //Main Watermark
+global.wm2 = '¸„٭⊹•~⍣°”ˆ˜¨ 𝐻𝑒𝑛𝑑𝑟𝑎-ᴍᴅ᭄࿐¨˜ˆ”°⍣~•⊹٭„¸'
+global.wm3 = '☘︎ 𝐻𝑒𝑛𝑑𝑟𝑎-ᴍᴅ᭄࿐'
 global.botdate = `☘︎ 𝗗𝗮𝘁𝗲: ${week} ${date}\n☘︎ 𝗧𝗶𝗺𝗲: ${wktuwib}`
 global.bottime = `𝗧 𝗜 𝗠 𝗘 : ${wktuwib}`
 global.titlebot = `☘︎ 𝗪𝗵𝗮𝘁𝘀𝗮𝗽𝗽 𝗕𝗼𝘁 𝗠𝘂𝗹𝘁𝗶-𝗗𝗲𝘃𝗶𝗰𝗲| By ${global.nameown}`
-global.author = '┄┄┄┅┅❑ 𝗕𝗮𝗿𝗮𝗫𝗗 ❑┅┅┄┄┄'
+global.author = '┄┄┄┅┅❑ 𝐻𝑒𝑛𝑑𝑟𝑎-ᴍᴅ᭄࿐ ❑┅┅┄┄┄'
 
 
 /*============== LOGO ==============*/
